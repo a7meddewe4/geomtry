bad2738ef35987eebc9c2a649db0312d805c79b8
ahmeddewedar <ibhdw2015@gmail.com>
2024-09-26 19:22:30 +0300

    new update

--- four_point_in_parallelogram.cpp
+++ four_point_in_parallelogram.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 int main() {
   double x1,y1,x2,y2,x3,y3,x4,y4;cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
-  if(x1==x3&&y3==y1)
+  // this is a git repositry for you
+  if(x1==x3&&y3==y1) 
   {
       double xa,xb,ya,yb;
       xa=x2-x1;
